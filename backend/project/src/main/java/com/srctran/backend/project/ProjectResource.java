60c01a5b3d91357b1c7705758b0f069c87dc854e
Thomas Feng <tfeng@linkedin.com>
2013-08-04 20:46:16 -0700

    Set up project with spring and jersey.

--- backend/project/src/main/java/com/srctran/backend/project/ProjectResource.java
+++ backend/project/src/main/java/com/srctran/backend/project/ProjectResource.java
@@ -1,5 +1,6 @@
 package com.srctran.backend.project;
 
+import javax.inject.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -7,6 +8,7 @@ import javax.ws.rs.core.MediaType;
 
 import com.srctran.backend.project.data.Project;
 
+@Singleton
 @Path("project")
 public class ProjectResource {
 
